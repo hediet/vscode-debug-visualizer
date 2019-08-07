58f9c8d37c3ef36564d75facf50cfeffcb4ba23b
Henning Dieterichs <henning.dieterichs@live.de>
2019-08-08 01:29:13 +0200

    Configures webpack to bundle the extension.

--- extension/webpack.config.ts
+++ extension/webpack.config.ts
@@ -1,14 +1,37 @@
 import * as webpack from "webpack";
 import path = require("path");
-import HtmlWebpackPlugin = require("html-webpack-plugin");
-import MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-import ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
 
 const r = (file: string) => path.resolve(__dirname, file);
 
 module.exports = {
 	target: "node",
 	entry: r("./src/extension"),
+	output: {
+		path: r("./dist"),
+		filename: "extension.js",
+		libraryTarget: "commonjs2",
+		devtoolModuleFilenameTemplate: "../[resource-path]",
+	},
+	devtool: "source-map",
+	externals: {
+		vscode: "commonjs vscode",
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "ts-loader",
+					},
+				],
+			},
+		],
+	},
+	plugins: [new CleanWebpackPlugin()],
 } as webpack.Configuration;
-
-// todo
