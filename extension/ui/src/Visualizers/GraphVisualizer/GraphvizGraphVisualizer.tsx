dcdb1c43d5d97ccc4da11bf01082e55cce5626e5
Henning Dieterichs <henning.dieterichs@live.de>
2020-02-20 13:51:00 +0100

    Fixes graphviz graph visualizer (undefined labels).

--- extension/ui/src/Visualizers/GraphVisualizer/GraphvizGraphVisualizer.tsx
+++ extension/ui/src/Visualizers/GraphVisualizer/GraphvizGraphVisualizer.tsx
@@ -36,8 +36,8 @@ export class GraphvizGraphVisualizer extends VisualizationProvider {
 
 @observer
 export class GraphvizGraphViewer extends React.Component<{
-	nodes: { id: string; label: string }[];
-	edges: { from: string; to: string; label: string }[];
+	nodes: { id: string; label?: string }[];
+	edges: { from: string; to: string; label?: string }[];
 }> {
 	render() {
 		const { nodes, edges } = this.props;
@@ -45,7 +45,10 @@ export class GraphvizGraphViewer extends React.Component<{
             digraph MyGraph {
                 ${nodes
 					.map(
-						n => `"${n.id}" [ label = ${JSON.stringify(n.label)} ];`
+						n =>
+							`"${n.id}" [ label = ${JSON.stringify(
+								n.label !== undefined ? n.label : n.id
+							)} ];`
 					)
 					.join("\n ")}
                 ${edges
@@ -53,7 +56,9 @@ export class GraphvizGraphViewer extends React.Component<{
 						e =>
 							`"${e.from}" -> "${
 								e.to
-							}" [ label = ${JSON.stringify(e.label)} ];`
+							}" [ label = ${JSON.stringify(
+								e.label !== undefined ? e.label : ""
+							)} ];`
 					)
 					.join("\n")}
             }
