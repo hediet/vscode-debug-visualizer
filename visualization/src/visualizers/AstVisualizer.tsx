67995ecbb814f1faa783ac5eca7f00cfe02b64cd
almenon <almenon214@gmail.com>
2020-03-22 14:43:13 -0700

    fix TS2497 error

--- visualization/src/visualizers/AstVisualizer.tsx
+++ visualization/src/visualizers/AstVisualizer.tsx
@@ -15,7 +15,7 @@ import { observer, disposeOnUnmount } from "mobx-react";
 import { observable, autorun, trace } from "mobx";
 import * as monaco from "monaco-editor";
 import { getLanguageId } from "./text-visualizers/MonacoTextVisualizer";
-import * as LineColumn from "line-column";
+import LineColumn from "line-column";
 
 export class AstVisualizer extends Visualizer {
 	visualize(data: ExtractedData, collector: VisualizationCollector): void {
