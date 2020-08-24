2f44c2d6f831dcfe096a613a7cd049601be33897
Henning Dieterichs <henning.dieterichs@live.de>
2020-08-24 17:05:31 +0200

    Fixes font csp issues.

--- extension/src/webview/InternalWebviewManager.ts
+++ extension/src/webview/InternalWebviewManager.ts
@@ -90,11 +90,11 @@ function getDebugVisualizerWebviewHtml(
         <html>
 			<head>
 			<meta charset="UTF-8">
-			<meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline'; worker-src * data: blob: data: 'unsafe-inline' 'unsafe-eval'; font-src * 'unsafe-inline' 'unsafe-eval';">
+			<meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline'; worker-src * data: blob: data: 'unsafe-inline' 'unsafe-eval'; font-src * 'unsafe-inline' 'unsafe-eval' 'self' data: blob:;">
             <style>
                 html { height: 100%; width: 100%; padding: 0; margin: 0; }
                 body { height: 100%; width: 100%; padding: 0; margin: 0; }
-                iframe { height: 100%; width: 100%; padding: 0; margin: 0; border: 0; display: block; }
+				iframe { height: 100%; width: 100%; padding: 0; margin: 0; border: 0; display: block; }
             </style>
             </head>
 			<body>
