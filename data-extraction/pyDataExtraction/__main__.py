607d6814e66e404c003ea46b3354c41d92c6cd70
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-09-02 10:30:32 -0500

    added comment to __main__ for clarification

--- data-extraction/pyDataExtraction/__main__.py
+++ data-extraction/pyDataExtraction/__main__.py
@@ -5,6 +5,11 @@ from abc import ABC
 from pyDataExtraction.commonTypes.Graph import Graph
 
 if __name__ == "__main__":
+    # NOTE this will likely be what will be called by the PyEvaluation Engine,
+    # this will be where this library interfaces with the existing codebase
+
+    # TODO: implement testing for each dataType, it may be a good idea to compare
+    # with output of typescript extractors
     graph_data1 = {"A": ["B", "C"], "B": ["A", "C"], "C": ["A", "D"], "D": ["A"]}
     graph_data2 = {1: [2, 3], 2: [1, 3], 3: [1, 4], 4: [1]}
     graph = Graph(graph_data1)
