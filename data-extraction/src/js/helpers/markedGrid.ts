28d02ca4ab46d0241e61b3c6e02482a3818ac367
Henning Dieterichs <henning.dieterichs@live.de>
2020-10-06 23:33:10 +0200

    Fixes marked grid.

--- data-extraction/src/js/helpers/markedGrid.ts
+++ data-extraction/src/js/helpers/markedGrid.ts
@@ -1,16 +1,18 @@
 import { GridVisualizationData } from "../../CommonDataTypes";
 
 export function markedGrid(
-	arr: any[],
-	marked: Record<string, number>
+	arr: unknown[],
+	marked: Record<string, number | undefined>
 ): GridVisualizationData {
 	return {
 		kind: { grid: true },
-		rows: [{ columns: arr.map(d => ({ tag: d })) }],
-		markers: Object.entries(marked).map(([key, val]) => ({
-			id: key,
-			row: 0,
-			column: val,
-		})),
+		rows: [{ columns: arr.map(d => ({ content: "" + d, tag: "" + d })) }],
+		markers: Object.entries(marked)
+			.map(([key, val]) => ({
+				id: "" + key,
+				row: 0,
+				column: val!,
+			}))
+			.filter(c => c.column !== undefined),
 	};
 }
